9bc21009584909abc978fd39b20260107edb8dfa
Yash Srivastava <yash33srivastava@gmail.com>
2021-07-12 19:17:17 +0530

    Update real_time_classification.py

--- real_time_classification.py
+++ real_time_classification.py
@@ -1,7 +1,8 @@
 import pandas as pd 
 import numpy as np
 
-df = pd.read_csv('data.csv')
+print("LOADING DATASET")
+df = pd.read_csv('data_test.csv')
 features=df.iloc[:,0:4]
 target=df.iloc[:,4:5]
 np.random.seed(1234)
@@ -14,15 +15,17 @@ feature = column_names
 
 from sklearn import tree
 decision_tree = tree.DecisionTreeClassifier(criterion='gini')
+print("APPLYING ML")
 decision_tree.fit(X, y)
-
 
 import serial
 import time
+import threading
 arduino_port = "/dev/ttyACM0" 
 baud = 115200 
 ser = serial.Serial(arduino_port, baud)
 print("Connected to Arduino port:" + arduino_port)
+print("READY")
 
 command_dict = {"front":'f', "back":'b', "left":'l', "right":'r', "stop":'s'}
 while True:
@@ -30,11 +33,11 @@ while True:
     raw_data = getData[2:][:-5]
     data = [int(x) for x in raw_data.split(',') if x.strip().isdigit()]
     if len(data) == 4:    
-        data = np.array(data)
-        data = data.reshape(1,-1)
+        data = (np.array(data)).reshape(1,-1)
+        #print("For ",data ," command is ", command)
         command = decision_tree.predict(data)
-        print("For ",data ," command is ", command)
         command_byte = command_dict[command[0]].encode('utf-8')
         ser.write(command_byte)
-    else:
-        print("Raw data received: ", raw_data)
+
+
+
